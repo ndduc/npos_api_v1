b782a38bd9fcade36a2a6f49e5961023600b6b77
ndduc <ndduc1856@gmail.com>
2021-12-12 17:39:26 -0700

    added template

--- POS_Api/Repository/Implementation/LocationRepos.cs
+++ POS_Api/Repository/Implementation/LocationRepos.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using POS_Api.Core.Interface;
+using POS_Api.Database.MySql.Configuration;
+using POS_Api.Model;
+using POS_Api.Repository.Interface;
+using POS_Api.Shared.DbHelper;
+using POS_Api.Shared.ExceptionHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+
 
 namespace POS_Api.Repository.Implementation
 {
-    public class LocationRepos
+    public class LocationRepos : BaseHelper
     {
     }
 }
