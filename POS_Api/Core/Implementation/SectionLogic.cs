b987fb73718f5e5c26a462a4c7d3b3b8d0887ac7
ndduc <ndduc1856@gmail.com>
2021-12-12 18:10:06 -0700

    added section repos

--- POS_Api/Core/Implementation/SectionLogic.cs
+++ POS_Api/Core/Implementation/SectionLogic.cs
@@ -18,10 +18,12 @@ namespace POS_Api.Core.Implementation
     {
         private readonly IUserLogic _userLogic;
         private readonly ILocationRepos _locationRepos;
+        private readonly ISectionRepos _sectionRepos;
         public SectionLogic(IUserLogic userLogic)
         {
             _userLogic = userLogic;
             _locationRepos = new LocationRepos();
+            _sectionRepos = new SectionRepos();
         }
 
         public bool UpdateSection(SectionModel model, string userId, string locationId)
@@ -40,46 +42,15 @@ namespace POS_Api.Core.Implementation
             }
 
             model.UpdatedBy = userId;
-            return UpdateSectionExecution(model);
+            return _sectionRepos.UpdateSectionExecution(model);
         }
 
-        private bool UpdateSectionExecution(SectionModel model)
-        {
-            int res = 0;
-            Conn = new DBConnection();
-            string query = " UPDATE asset_section "
-                        + " SET "
-                        + " `description` = " + DbHelper.SetDBValue(model.Description, false)
-                        + " `updated_by` = " + DbHelper.SetDBValue(model.UpdatedBy, true)
-                        + " WHERE "
-                        + " uid = " + DbHelper.SetDBValue(model.UId, true) + " AND "
-                        + " location_uid = " + DbHelper.SetDBValue(model.LocationUId, true) + "";
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    res = Cmd.ExecuteNonQuery();
-                    Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
-
-            return CheckUpdateHelper(res);
-        }
 
         public List<SectionModel> GetSectionByLocationId(string userId, string locationId)
         {
             if (_userLogic.VerifyUser(userId) && _locationRepos.VerifyUIdExist(locationId))
             {
-                return GetSectionByLocationIdExecution(locationId);
+                return _sectionRepos.GetSectionByLocationIdExecution(locationId);
             }
             else
             {
@@ -87,60 +58,6 @@ namespace POS_Api.Core.Implementation
             }
         }
 
-        private List<SectionModel> GetSectionByLocationIdExecution(string locationId)
-        {
-            List<SectionModel> lst = new List<SectionModel>();
-            Conn = new DBConnection();
-            string query = " SELECT * FROM asset_section "
-                                + " WHERE `location_uid` = "
-                                + DbHelper.SetDBValue(locationId, true)
-                                + " ORDER BY `description` ASC; ";
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    Reader = Cmd.ExecuteReader();
-                    while (Reader.Read())
-                    {
-                        SectionModel model = new SectionModel()
-                        {
-                            UId = DbHelper.TryGet(Reader, "uid"),
-                            Description = DbHelper.TryGet(Reader, "description"),
-                            LocationUId = DbHelper.TryGet(Reader, "location_uid"),
-                            AddedDateTime = DbHelper.TryGet(Reader, "added_datetime"),
-                            UpdatedDateTime = DbHelper.TryGet(Reader, "updated_datetime"),
-                            AddedBy = DbHelper.TryGet(Reader, "added_by"),
-                            UpdatedBy = DbHelper.TryGet(Reader, "updated_by"),
-                        };
-
-                        lst.Add(model);
-                    }
-                    this.Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
-
-            if (lst.Count > 0)
-            {
-                return lst;
-            }
-            else
-            {
-                SectionModel model = new SectionModel();
-                model.IsError = true;
-                model.Error = "No Section Found";
-                lst.Add(model);
-                return lst;
-            }
-        }
 
 
         public bool AddSection(SectionModel model, string userId, string locationId)
@@ -150,14 +67,14 @@ namespace POS_Api.Core.Implementation
             while (!isUnqiue)
             {
                 id = Guid.NewGuid().ToString();
-                isUnqiue = VerifyUIdUnique(id);
+                isUnqiue = _sectionRepos.VerifyUIdUnique(id);
             }
             if (_userLogic.VerifyUser(userId) && _locationRepos.VerifyUIdExist(locationId))
             {
                 model.UId = id;
                 model.AddedBy = userId;
                 model.LocationUId = locationId;
-                return AddSectionExecution(model);
+                return _sectionRepos.AddSectionExecution(model);
             }
             else
             {
@@ -165,48 +82,17 @@ namespace POS_Api.Core.Implementation
             }
         }
 
-        private bool AddSectionExecution(SectionModel model)
-        {
-            int res = 0;
-            Conn = new DBConnection();
-            string query = "INSERT INTO asset_Section "
-                            + " (`uid`,`description`, `location_uid`, `added_by`) "
-                            + " VALUES ("
-                            + DbHelper.SetDBValue(model.UId, false)
-                            + DbHelper.SetDBValue(model.Description, false)
-                            + DbHelper.SetDBValue(model.LocationUId, false)
-                            + DbHelper.SetDBValue(model.AddedBy, true)
-                            + " ); ";
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    res = Cmd.ExecuteNonQuery();
-                    Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
 
-            return CheckInsertionHelper(res);
-        }
 
         public bool AddSectionProductRelation(string uid, string productId, string locationId, string userId)
         {
             if (_userLogic.VerifyUser(userId)
                 && _locationRepos.VerifyUIdExist(locationId)
-                && VerifyUIdExist(uid)
-                && !VerifySectionProductRelationExist(uid, productId, locationId))
+                && _sectionRepos.VerifyUIdExist(uid)
+                && !_sectionRepos.VerifySectionProductRelationExist(uid, productId, locationId))
             {
 
-                return AddSectionProductRelationExecution(uid, productId, locationId, userId);
+                return _sectionRepos.AddSectionProductRelationExecution(uid, productId, locationId, userId);
             }
             else
             {
@@ -214,124 +100,6 @@ namespace POS_Api.Core.Implementation
             }
         }
 
-        private bool AddSectionProductRelationExecution(string uid, string productId, string locationId, string userId)
-        {
-            int res = 0;
-            Conn = new DBConnection();
-            string query = " INSERT INTO ref_location_product_Section "
-                            + " (`product_uid`, `location_uid`, `Section_uid`, `added_by`) "
-                            + " VALUE ( "
-                            + DbHelper.SetDBValue(productId, false)
-                            + DbHelper.SetDBValue(locationId, false)
-                            + DbHelper.SetDBValue(uid, false)
-                            + DbHelper.SetDBValue(userId, true)
-                            + " ); ";
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    res = Cmd.ExecuteNonQuery();
-                    Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
 
-            return CheckInsertionHelper(res);
-        }
-
-        private bool VerifySectionProductRelationExist(string uid, string productId, string locationId)
-        {
-            this.Conn = new DBConnection();
-            string id = null;
-            string query = " SELECT id FROM ref_location_product_Section "
-                            + " WHERE "
-                            + " product_uid = " + DbHelper.SetDBValue(productId, true) + " AND "
-                            + " location_uid = " + DbHelper.SetDBValue(locationId, true) + " AND "
-                            + " Section_uid = " + DbHelper.SetDBValue(uid, true) + " ; ";
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    Reader = Cmd.ExecuteReader();
-                    while (Reader.Read())
-                    {
-                        id = DbHelper.TryGet(Reader, "id");
-                    }
-                    this.Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
-            return CheckExistingHelper(id);
-        }
-
-        private bool VerifyUIdUnique(string uid)
-        {
-            this.Conn = new DBConnection();
-            string id = null;
-            string query = "SELECT uid FROM asset_Section WHERE uid = " + DbHelper.SetDBValue(uid, true) + ";";
-
-            try
-            {
-                if (Conn.IsConnect())
-                {
-                    Cmd = new MySqlCommand(query, this.Conn.Connection);
-                    Reader = Cmd.ExecuteReader();
-                    while (Reader.Read())
-                    {
-                        id = DbHelper.TryGet(Reader, "uid");
-                    }
-                    this.Conn.Close();
-                }
-                else
-                {
-                    throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-                }
-
-            }
-            catch (Exception e)
-            {
-                throw GenericException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name, e.ToString()));
-            }
-            return VerifyNotExist(id);
-        }
-
-        public bool VerifyUIdExist(string uid)
-        {
-            Conn = new DBConnection();
-            string id = null;
-            string query = "SELECT uid FROM asset_Section WHERE uid = " + DbHelper.SetDBValue(uid, true) + ";";
-            if (Conn.IsConnect())
-            {
-                Cmd = new MySqlCommand(query, this.Conn.Connection);
-                Reader = Cmd.ExecuteReader();
-                while (Reader.Read())
-                {
-                    id = DbHelper.TryGet(Reader, "uid");
-                }
-                Conn.Close();
-            }
-            else
-            {
-                throw DbConnException(GenerateExceptionMessage(GetType().Name, MethodBase.GetCurrentMethod().Name));
-            }
-            return CheckExistingHelper(id);
-        }
     }
 }
--- POS_Api/Repository/Interface/ISectionRepos.cs
+++ POS_Api/Repository/Interface/ISectionRepos.cs
@@ -13,7 +13,14 @@ using System.Threading.Tasks;
 
 namespace POS_Api.Repository.Interface
 {
-    interface ISectionRepos
+    public interface ISectionRepos
     {
+        public bool AddSectionProductRelationExecution(string uid, string productId, string locationId, string userId);
+        public bool VerifySectionProductRelationExist(string uid, string productId, string locationId);
+        public bool VerifyUIdUnique(string uid);
+        public bool VerifyUIdExist(string uid);
+        public bool AddSectionExecution(SectionModel model);
+        public List<SectionModel> GetSectionByLocationIdExecution(string locationId);
+        public bool UpdateSectionExecution(SectionModel model);
     }
 }
