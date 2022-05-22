7f922d88cfabfdb1c7cb0ecc0781f5f7260c0298
ndduc <ndduc1856@gmail.com>
2022-05-22 10:53:21 -0700

    added sub cat model

---
+++ POS_Api/Model/SubCategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POS_Api.Model
+{
+    public class SubCategoryModel
+    {
+        public string UId { get; set; }
+        public string Description { get; set; }
+        public string SecondDescription { get; set; }
+        public bool ApplyToUI { get; set; }
+        public string AddedDateTime { get; set; }
+        public string UpdatedDateTime { get; set; }
+        public string CategoryUId { get; set; }
+        public string LocationUId { get; set; }
+        public string AddedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsError { get; set; }
+        public string Error { get; set; }
+    }
+}
