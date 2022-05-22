166bfd85e3536ae03e22cc1991313a81a84ab489
ndduc <ndduc1856@gmail.com>
2022-05-22 11:14:43 -0700

    added category interface

---
+++ POS_Api/Core/Interface/ISubCategoryLogic.cs
@@ -0,0 +1,24 @@
+﻿using POS_Api.Model;
+using System.Collections.Generic;
+
+namespace POS_Api.Core.Interface
+{
+    public interface ICategoryLogic
+    {
+        public bool AddCategory(CategoryModel model, string userId, string locationId);
+        public bool AddCategoryProductRelation(string uid, string productId, string locationId, string userId);
+
+        public List<CategoryModel> GetCategoryByLocationId(string userId, string locationId);
+
+        public bool UpdateCategory(CategoryModel model, string userId, string locationId);
+
+        public int GetCategoryPaginateCount(Dictionary<string, string> param);
+        public IEnumerable<CategoryModel> GetCategoryPaginate(Dictionary<string, string> param);
+
+        public CategoryModel GetCategoryById(string userId, string locId, string CategoryId);
+
+        public IEnumerable<CategoryModel> GetCategoryByDescription(string userId, string locId, string description);
+
+        public IEnumerable<CategoryModel> GetCategoryByDepartmentId(string userId, string locId, string deptId);
+    }
+}
